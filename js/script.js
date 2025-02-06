1e4cfd766c79a8a9c2744f6af406d899a3e1c32e
Sasha <flopsilove@gmail.com>
2025-02-06 18:40:18 +0200

    .

--- js/script.js
+++ js/script.js
@@ -1,56 +1,52 @@
-// Modal
-!function(e){"function"!=typeof e.matches&&(e.matches=e.msMatchesSelector||e.mozMatchesSelector||e.webkitMatchesSelector||function(e){for(var t=this,o=(t.document||t.ownerDocument).querySelectorAll(e),n=0;o[n]&&o[n]!==t;)++n;return Boolean(o[n])}),"function"!=typeof e.closest&&(e.closest=function(e){for(var t=this;t&&1===t.nodeType;){if(t.matches(e))return t;t=t.parentNode}return null})}(window.Element.prototype);
-
+//Modal
 document.addEventListener('DOMContentLoaded', function() {
-
     var modalButtons = document.querySelectorAll('.js-open-modal'),
-        overlay      = document.querySelector('.modal-overlay'),
-        closeButtons = document.querySelectorAll('.js-close-modal');
+        overlay = document.querySelector('.modal-overlay'),
+        closeButtons = document.querySelectorAll('.js-close-modal'),
+        header = document.querySelector('header');  // находим хедер
 
     modalButtons.forEach(function(item){
-
-    item.addEventListener('click', function(e) {
-
-        e.preventDefault();
-
-        var modalId = this.getAttribute('data-modal'),
-            modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]');
-            form = modalElem.querySelector('form');
+        item.addEventListener('click', function(e) {
+            e.preventDefault();
+            var modalId = this.getAttribute('data-modal'),
+                modalElem = document.querySelector('.modal[data-modal="' + modalId + '"]'),
+                form = modalElem.querySelector('form');
             
-        modalElem.classList.add('active');
-        overlay.classList.add('active');
+            modalElem.classList.add('active');
+            overlay.classList.add('active');
+            header.style.display = 'none';  // скрываем хедер
 
-        if (form) {
-            form.reset();
-        }
-    }); 
-}); 
+            if (form) {
+                form.reset();
+            }
+        });
+    });
 
     closeButtons.forEach(function(item){
-
-    item.addEventListener('click', function(e) {
-        var parentModal = this.closest('.modal');
-
-        parentModal.classList.remove('active');
-        overlay.classList.remove('active');
+        item.addEventListener('click', function(e) {
+            var parentModal = this.closest('.modal');
+            parentModal.classList.remove('active');
+            overlay.classList.remove('active');
+            header.style.display = 'block';  // показываем хедер
+        });
     });
-}); 
 
     document.body.addEventListener('keyup', function (e) {
         var key = e.keyCode;
-
         if (key == 27) {
-
             document.querySelector('.modal.active').classList.remove('active');
-            document.querySelector('.modal-overlay').classList.remove('active');
+            overlay.classList.remove('active');
+            header.style.display = 'block';  // показываем хедер
         };
     }, false);
 
     overlay.addEventListener('click', function() {
         document.querySelector('.modal.active').classList.remove('active');
         this.classList.remove('active');
+        header.style.display = 'block';  // показываем хедер
     });
-}); 
+});
+
 
 
 // Burger
